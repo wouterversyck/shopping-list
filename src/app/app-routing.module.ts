34aa733c2a47dcf7fcf137106b3b9ff14cf7a65f
WouterVersyck <wouter.versyck@gmail.com>
2019-07-07 13:43:51 +0200

    Added login module with lazy loaded routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
