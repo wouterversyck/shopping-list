cb572f17f231c7e4b4bcef17a103dffd8d5a655f
Wouter Versyck <wouter.versyck@gmail.com>
2020-02-01 15:34:20 +0100

    Update angular material
    Update angular material to 9.x

--- src/app/modules/core/material/material.module.ts
+++ src/app/modules/core/material/material.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule, MatCardModule,
-  MatFormFieldModule, MatPaginatorModule,
-  MatIconModule, MatInputModule,
-  MatListModule, MatProgressSpinnerModule,
-  MatMenuModule, MatTableModule, MatTabsModule,
-  MatToolbarModule, MatSnackBarModule,
-  MatAutocompleteModule, MatGridListModule
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
