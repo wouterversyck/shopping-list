75acfabede87a4da8300175115f714a7bf404261
Wouter Versyck <wouter.versyck@gmail.com>
2020-08-08 14:42:52 +0200

    Server sync icon finished

--- src/app/modules/notes/components/note/note.component.ts
+++ src/app/modules/notes/components/note/note.component.ts
@@ -13,32 +13,44 @@ import { RichTextComponent } from '@app/modules/notes/components/note/entries/ri
 import { CheckListComponent } from '@app/modules/notes/components/note/entries/check-list/check-list.component';
 import { AbstractControl, FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { NotesService } from '@app/modules/notes/services/notes/notes.service';
-import { debounceTime, map, switchMap } from 'rxjs/operators';
+import { debounceTime, finalize, map, switchMap, tap } from 'rxjs/operators';
 import { EntryType } from '@app/modules/notes/models/entry-type.model';
 import { LinkComponent } from '@app/modules/notes/components/note/entries/link/link.component';
 import { RichText } from '@app/modules/notes/models/rich-text.model';
 import { CheckList } from '@app/modules/notes/models/check-list.model';
 import { LinkPreview } from '@app/modules/notes/models/link-preview.model';
 import { Entry } from '@app/modules/notes/models/entry.model';
-import { Subscription } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.scss']
+  styleUrls: ['./note.component.scss'],
+  animations: [
+    trigger('showSyncIndicator', [
+      state('show' , style({ opacity: 1 })),
+      state('hide', style({ opacity: 0 })),
+      transition('hide => show', animate('.1s')),
+      transition('show => hide', animate('.25s 1s ease-out'))
+    ])
+  ]
 })
 export class NoteComponent implements OnInit, OnDestroy {
+  @Input() editMode: boolean;
   @Input() note: Note;
   form: FormGroup;
   @ViewChild('list', {static: true}) listHtml: HTMLElement;
   @ViewChild(AddHostDirective, {static: true}) adHost: AddHostDirective;
 
+  dirty = false;
+
   subs: Subscription[] = [];
 
-  entryMap = new Map<EntryType, any>([
-    [EntryType.CHECK_LIST, { component: CheckListComponent, entry: CheckList }],
-    [EntryType.RICH_TEXT, { component: RichTextComponent, entry: RichText }],
-    [EntryType.LINK, { component: LinkComponent, entry: LinkPreview }]
+  entryMap = new Map<EntryType, { component: any , entry: () => Entry }>([
+    [EntryType.CHECK_LIST, { component: CheckListComponent, entry: () => new CheckList() }],
+    [EntryType.RICH_TEXT, { component: RichTextComponent, entry: () => new RichText() }],
+    [EntryType.LINK, { component: LinkComponent, entry: () => new LinkPreview() }]
   ]);
 
   fabButtons = [
@@ -65,18 +77,18 @@ export class NoteComponent implements OnInit, OnDestroy {
       this.subs.forEach(sub => sub.unsubscribe());
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     const forms = this.constructDynamicComponents();
     this.constructForm(this.formBuilder.array(forms));
     this.wireFormToService();
   }
 
   addItemToViewAndSyncServer(type: EntryType) {
-      const entry = new (this.entryMap.get(type).entry)();
+      const entry = this.entryMap.get(type).entry();
       this.formItems.push(this.addEntry(entry));
   }
 
-  private constructForm(forms: FormArray) {
+  private constructForm(forms: FormArray): void {
     this.form = this.formBuilder.group({
       name: this.note.name,
       id: this.note.id,
@@ -94,19 +106,21 @@ export class NoteComponent implements OnInit, OnDestroy {
     return [];
   }
 
-  private wireFormToService() {
+  private wireFormToService(): void {
     this.subs.push(this.form.valueChanges
       .pipe(
+        tap(() => this.dirty = true),
         debounceTime(500),
         map(e => {
           e.contributors = e.contributors.map(d => d.id);
           return e;
         }),
-        switchMap(this.notesService.save)
+        switchMap(this.notesService.save),
+        tap(() => this.dirty = false)
       ).subscribe());
   }
 
-  private constructDynamicComponents() {
+  private constructDynamicComponents(): FormGroup[] {
     if (!this.note.items) {
       return;
     }
@@ -115,7 +129,7 @@ export class NoteComponent implements OnInit, OnDestroy {
       .map(e => this.addEntry(e));
   }
 
-  private addEntry(entry: Entry) {
+  private addEntry(entry: Entry): FormGroup {
     const componentRef = this.getComponentFromEntry(entry.entryType);
     const component = componentRef.instance as NoteEntry;
 
@@ -134,21 +148,22 @@ export class NoteComponent implements OnInit, OnDestroy {
     return this.adHost.viewContainerRef.createComponent(componentFactory);
   }
 
-  private moveComponentDown(hostView: ViewRef) {
+  private moveComponentDown(hostView: ViewRef): void {
     const previousIndex = this.adHost.viewContainerRef.indexOf(hostView);
     const newIndex = previousIndex < this.adHost.viewContainerRef.length - 1 ? previousIndex + 1 : previousIndex;
 
     this.moveFormItemAndComponent(previousIndex, newIndex, this.formItems.at(previousIndex), hostView);
   }
 
-  private moveComponentUp(hostView: ViewRef) {
+  private moveComponentUp(hostView: ViewRef): void {
     const previousIndex = this.adHost.viewContainerRef.indexOf(hostView);
     const newIndex = previousIndex > 0 ? previousIndex - 1 : previousIndex;
 
     this.moveFormItemAndComponent(previousIndex, newIndex, this.formItems.at(previousIndex), hostView);
   }
 
-  private moveFormItemAndComponent(previousIndex: number, newIndex: number, formItem: AbstractControl, viewRef: ViewRef) {
+  private moveFormItemAndComponent(
+    previousIndex: number, newIndex: number, formItem: AbstractControl, viewRef: ViewRef): void {
     if (previousIndex === newIndex) {
       return;
     }
@@ -158,7 +173,7 @@ export class NoteComponent implements OnInit, OnDestroy {
     this.formItems.insert(newIndex, formItem);
   }
 
-  private delete(hostView: ViewRef) {
+  private delete(hostView: ViewRef): void {
     const index = this.adHost.viewContainerRef.indexOf(hostView);
     this.adHost.viewContainerRef.remove(index);
     this.formItems.removeAt(index);
